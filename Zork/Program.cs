f2179656579ebd6a4b9482c8b80601720b1ed7a5
BrotherMutton <matthew.wisner@knights.ucf.edu>
2020-08-31 14:44:12 -0400

    Zork 1.1 - Looking Around and Quitting
    yes sir this here is looking around and quitting

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Zork
 {
@@ -7,6 +8,23 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+
+            //bool result = string.Compare(inputString, "QUIT", true) == 0;
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing!");
+            }
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat reading \"Welcome to Zork\" lies by the door.");
+            }
+            else
+            {
+                Console.WriteLine("That's not a verb I recognize.");
+            }
         }
     }
 }
