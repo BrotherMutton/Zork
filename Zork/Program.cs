fae9f5c3c8760299e17744f66bb3cfe053372346
BrotherMutton <matthew.wisner@knights.ucf.edu>
2020-09-02 15:28:25 -0400

    Zork 1.3 - The Game Loop
    adding the game loop and allowing players to input all our commands

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,16 +9,47 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:                        
+                    case Commands.SOUTH:                        
+                    case Commands.EAST:                       
+                    case Commands.WEST:
+                        outputString = $"You move {command}.";
+                        break;
+
+                    case Commands.UNKNOWN:
+                        outputString = "Unknown command.";
+                        break;
+
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+                Console.WriteLine(outputString);
+
+            }     
                      
         }
 
-
         private static Commands ToCommand(string commandString)
         {
-            if (Enum.TryParse<Commands>(commandString, true, out Commands result))
+            if (Enum.TryParse<Commands>(commandString, ignoreCase: true, out Commands result))
             {
                 return result;
             }
